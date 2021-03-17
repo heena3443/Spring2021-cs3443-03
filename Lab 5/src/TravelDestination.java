c62aa3f2be2fc739dccbb32af73718f89b0e90e5
Brando <branmuller@gmail.com>
2021-03-16 19:00:52 -0500

    Added a print statement for my travel introduction

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -3,6 +3,7 @@ public class TravelDestination {
 
 	public static void main(String[] args) {
 		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
+		System.out.println("My name is Brandon Muller. I wish to travel to Italy to eat lots of pizza!");
 	}
 
 }
