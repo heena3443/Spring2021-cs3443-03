fe1fa1960ef18689e9f335454d6606bc68011968
huynhn98 <72099264+huynhn98@users.noreply.github.com>
2021-03-17 13:28:12 -0500

    Update TravelDestination.java
    added my destination

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -5,6 +5,7 @@ public class TravelDestination {
 		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
 		System.out.println("My name is Joseph Galvan. I wish to travel to Russia someday to learn more about the culture");
 		System.out.println("My name is Elijah Moya. I wish to travel to Mexico and finally see life outside the US.");
+		System.out.println("My name is Nam Huynh. I wish to travel to Japan to immerse myself in it's beautiful nature and traditions");
 	}
 
 }
