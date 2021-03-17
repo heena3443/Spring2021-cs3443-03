ef7958aad4c757c1b0821d75d72fe5f2134fb46a
Elijah762 <78748880+Elijah762@users.noreply.github.com>
2021-03-17 13:08:37 -0500

    Update TravelDestination.java
    My desired travel destination after covid is over

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -4,6 +4,7 @@ public class TravelDestination {
 	public static void main(String[] args) {
 		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
 		System.out.println("My name is Joseph Galvan. I wish to travel to Russia someday to learn more about the culture");
+		System.out.println("My name is Elijah Moya. I wish to travel to Mexico and finally see life outside the US.");
 	}
 
 }
