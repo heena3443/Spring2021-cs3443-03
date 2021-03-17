7f925d696fcaa5c079238c931b335470d72c8f26
Saul Carreon <sacarreon18@gmail.com>
2021-03-16 21:34:24 -0500

    Update TravelDestination.java

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -4,6 +4,7 @@ public class TravelDestination {
 	public static void main(String[] args) {
 		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
 		System.out.println("My name is Joseph Galvan. I wish to travel to Russia someday to learn more about the culture");
+		System.out.println("My name is Saul Carreon. I wish to travel to Japan to view their sights and visit Akihabara");
 	}
 
 }
