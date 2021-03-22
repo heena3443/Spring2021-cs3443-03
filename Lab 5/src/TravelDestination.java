b8345e55099e6f9e8c845bc396f8dbc584c5ecd6
inq968 <51177187+inq968@users.noreply.github.com>
2021-03-22 18:03:42 -0500

    Mahnoor Bokhari - travel destination

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -19,6 +19,7 @@ public class TravelDestination {
 		System.out.println("My name is Eamon Nava. I wish to travel to Hawaii to see sea life.");
 		System.out.println("My name is Darrian Aguilar-Rivera. I wish to travel to the Bahamas to see crytsal clear beaches.");
 		System.out.println("My name is Darrian Aguilar-Rivera. I wish to travel to the Bahamas to see crytsal clear beaches.");
+		System.out.println("My name is Mahnoor Bokhari. I wish to travel to a mountainous region anywhere in the world to enjoy the snow!");
 	}
 	
 }
