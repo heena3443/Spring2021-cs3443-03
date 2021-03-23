9b8e3d318cd22403c9094e250d72ce4dd4d97fdd
meera-patel01 <80857225+meera-patel01@users.noreply.github.com>
2021-03-23 15:21:41 -0500

    Update TravelDestination.java

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -31,7 +31,8 @@ public class TravelDestination {
 		System.out.println("My name is Brian van Vlymen. I wish to travel to moon with SpaceX!");
 		System.out.println("My name is Alejandro Garcia. I wish to travel to Japan to see its scenery");
 		System.out.println("My name is Victor Aguilar. I wish to travel to Norway since I've never been there!");
-		System.out.println("My name is Yaphet Samson, I wish to travel to The Netherlands to enjoy the architecture and culture"); 
+		System.out.println("My name is Yaphet Samson, I wish to travel to The Netherlands to enjoy the architecture and culture");
+		System.out.println("My name is Meera Patel, I wish to travel to Norway, Sweden, or Canada to see the northern lights.");
 	}
 	
 }
