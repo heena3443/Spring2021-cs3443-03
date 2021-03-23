9d64afdc6b9a2b053e6a1ee6450e40b9c13f55d4
Saul Carreon <sacarreon18@gmail.com>
2021-03-23 17:09:30 -0500

    Update TravelDestination.java
    I added my name and where I want to travel.

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -4,7 +4,7 @@ public class TravelDestination {
 	public static void main(String[] args) {
 		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
 		System.out.println("My name is Joseph Galvan. I wish to travel to Russia someday to learn more about the culture");
-		System.out.println("My name is Saul Carreon. I wish to travel to Japan to view their sights and visit Akihabara");
+		System.out.println("My name is Saul Carreon. I wish to travel to Japan to look at the scenery and go to Akihabara");
 	}
 
 }
